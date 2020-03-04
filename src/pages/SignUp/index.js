d92bca118ad03b950ff519c8ba7b1ade0d4a4eb9
Julio Cesar <julio.souzam@gmail.com>
2020-03-04 13:42:15 +0000

    feat: layouts, settings and routes

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('E-mail obrigatório'),
+  password: Yup.string()
+    .required('A senha é obrigatória')
+    .min(8, 'A senha deve ter 8 caracteres'),
+});
+
+export default function Signin() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
